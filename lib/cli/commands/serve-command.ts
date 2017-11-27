e016c89ec5cebfb0c3a2083c6d69ca4ec6cefbfc
Chuev, Aleksandr <a.chuev@itransition.com>
2017-11-27 20:44:27 +0300

    Fix a serve command path

--- lib/cli/commands/serve-command.ts
+++ lib/cli/commands/serve-command.ts
@@ -2,11 +2,12 @@ import { watchTree } from 'watch';
 import * as ls from 'live-server';
 import { build } from '../../build';
 import { loadOptions } from '../utils/load-options';
+import { Options } from '../../interfaces/options';
 
 export function serveCommand(command: any) {
   const options = loadOptions(command);
 
-  watchTree('static', () => build(options));
+  watchTree(options.src.folder, () => rebuild(options));
 
   ls.start({
     root: options.dist.folder,
@@ -14,4 +15,12 @@ export function serveCommand(command: any) {
     port: options.serve.port,
     open: options.serve.open,
   });
+}
+
+function rebuild(options: Options) {
+  console.log('\x1b[32m', '\nRebuilding...', '\x1b[0m');
+
+  build(options);
+
+  console.log('\x1b[32m', 'Rebuilt successfully.', '\x1b[0m');
 }
