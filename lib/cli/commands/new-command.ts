310c7ed8a7f53d45cc10961ebe154ca4488cf991
Alex Chuev <alex@chuev.com>
2017-11-28 00:37:24 +0300

    Fix new command src path

--- lib/cli/commands/new-command.ts
+++ lib/cli/commands/new-command.ts
@@ -2,5 +2,5 @@ import { generate } from '../generate/generate';
 import { CreateAnswers } from './answers/create';
 
 export function newCommand(dir: string, answers: CreateAnswers) {
-  generate(answers, {dir, src: ''});
+  generate(answers, {dir, src: 'src'});
 }
