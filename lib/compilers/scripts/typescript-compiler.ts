e431e78d3dae90b0d53d9a1caa74518d4d39781b
Alex Chuev <alex@chuev.com>
2017-12-09 00:54:50 +0300

    Add JS minification

--- lib/compilers/scripts/typescript-compiler.ts
+++ lib/compilers/scripts/typescript-compiler.ts
@@ -1,9 +1,10 @@
 import * as ts from 'typescript';
+import { minify } from 'uglify-js';
 
 import { Compiler } from '../compiler';
 
 export class TypescriptCompiler extends Compiler {
   method(src: string, data: any, file: string): string {
-    return ts.transpileModule(src, this.options.scripts.options).outputText;
+    return minify(ts.transpileModule(src, this.options.scripts.options).outputText).code;
   }
 }
