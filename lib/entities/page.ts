5fc2f75a19ba084914536b004d274e6e871a426c
Alex Chuev <alex@chuev.com>
2017-12-17 15:55:58 +0300

    Add some url helpers

--- lib/entities/page.ts
+++ lib/entities/page.ts
@@ -6,6 +6,7 @@ import { PageCode } from './page-code';
 import { Url } from '../types';
 import { createAbsoluteUrl } from '../factories/template-helpers-factory';
 import { Options } from '../interfaces/options';
+import * as path from 'path';
 
 export interface Page extends File {
   data: PageData;
@@ -30,15 +31,30 @@ export class PageData {
   js = new Code();
   language: Language;
   otherLanguages: Language[];
+  currentUrl: Url;
+  currentDefaultLanguageUrl: Url;
   assets: File[] = [];
-  asset: (filename: string) => Url;
+  asset: (relativeUrl: Url) => Url;
+  url: (relativeUrl: Url, languageName: string) => Url;
+  languageUrl: (languageName: string) => Url;
 
-  constructor(
-    props: PageDataProps,
-  ) {
+  constructor(props: PageDataProps) {
     this.language = props.language;
     this.id = replaceExtension(props.file.relative, '');
     this.otherLanguages = props.dependencies.languages.filter(language => language !== props.language);
-    this.asset = (filename: string) => createAbsoluteUrl(filename, props.options);
+
+    this.asset = (relativeUrl: Url) => asset(relativeUrl, props.options);
+    this.currentUrl = url(`${this.id}.html`, this.language.url, props.options);
+    this.currentDefaultLanguageUrl = createAbsoluteUrl(`${this.id}.html`, props.options);
+    this.url = (relativeUrl: Url, languageName = props.language.name) => url(relativeUrl, languageName, props.options);
+    this.languageUrl = (languageName: Url) => url(`${this.id}.html`, languageName, props.options);
   }
+}
+
+function asset(relativeUrl: Url, options: Options): Url {
+  return createAbsoluteUrl(relativeUrl, options);
+}
+
+function url(relativeUrl: Url, languageName: string, options: Options): Url {
+  return createAbsoluteUrl(path.join(Language.getUrl(languageName, options), relativeUrl), options);
 }
