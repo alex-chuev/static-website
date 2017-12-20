da250dbedc1775a9a067a26fcea2fc2e32be2ca6
Chuev, Aleksandr <a.chuev@itransition.com>
2017-12-21 00:22:27 +0300

    Implement building

--- lib/entities/page.ts
+++ lib/entities/page.ts
@@ -1,9 +1,8 @@
 import { Language } from './language';
 import * as File from 'vinyl';
-import { Code, CodeType } from './code';
+import { CodeType } from './code';
 import { Url } from '../types';
 import { createAbsoluteUrl } from '../helpers/url-helpers';
-import { Config } from '../interfaces/config';
 import { PropertyPath } from 'lodash';
 import { assetHelper, urlHelper } from '../helpers/template-helpers';
 import { removeExtension } from '../helpers/path-helpers';
@@ -11,31 +10,20 @@ import { Environment } from '../interfaces/environment';
 import { Attrs } from '../interfaces/attributes';
 import { HtmlFactory } from '../factories/html-factory';
 import { readFileSync } from "fs";
+import { BuildCache } from '../cache';
 
 export interface PageFile extends File {
   data: PageData;
 }
 
-export interface PageDataProps {
-  config: Config;
-  languages: Language[];
-  file: File;
-  language: Language;
-  environment: Environment;
-  code: Code;
-}
-
 export class PageData {
-  id: string;
   language: Language;
   css: CodeType;
   js: CodeType;
-  options: Config;
   otherLanguages: Language[];
   currentUrl: Url;
   environment: Environment;
   currentDefaultLanguageUrl: Url;
-  assets: File[] = [];
   asset: (relativeUrl: Url) => Url;
   url: (relativeUrl: Url, languageName: string) => Url;
   languageUrl: (languageName: string) => Url;
@@ -44,29 +32,40 @@ export class PageData {
   link: (url: Url, text?: string, className?: string, activeClass?: string, attrs?: Attrs, language?: string) => string;
   languageLink: (language: string, text?: string, className?: string, activeClass?: string, attrs?: Attrs) => string;
 
-  constructor(props: PageDataProps) {
-    this.language = props.language;
-    this.environment = props.environment;
-    this.options = props.config;
-    this.id = removeExtension(props.file.relative,);
-    this.otherLanguages = props.languages.filter(language => language !== props.language);
-    this.css = props.code.css;
-    this.js = props.code.js;
+  constructor(page: Page, language: Language, cache: BuildCache) {
+    this.language = language;
+    this.environment = cache.environment;
+    this.otherLanguages = cache.languages.filter(item => item !== language);
 
-    this.asset = (relativeUrl: Url) => assetHelper(relativeUrl, props.config);
-    this.currentUrl = urlHelper(`${this.id}.html`, this.language.url, props.config);
-    this.currentDefaultLanguageUrl = createAbsoluteUrl(`${this.id}.html`, props.config);
-    this.url = (relativeUrl: Url, languageName = props.language.name) => urlHelper(
-      relativeUrl, languageName, props.config);
-    this.languageUrl = (languageName: string) => urlHelper(`${this.id}.html`, languageName, props.config);
+    this.css = new CodeType();
+    this.css.external.push(cache.globalExternalCss, cache.pageExternalCss.get(page));
+    this.js = new CodeType();
+    this.js.external.push(cache.globalExternalJs, cache.pageExternalJs.get(page));
+
+    if (cache.environment.production) {
+      this.css.inline.push(cache.globalInlineCss, cache.pageInlineCss.get(page));
+      this.js.inline.push(cache.globalInlineJs, cache.pageInlineJs.get(page));
+    } else {
+      this.css.external.push(cache.globalInlineCss, cache.pageInlineCss.get(page));
+      this.js.external.push(cache.globalInlineJs, cache.pageInlineJs.get(page));
+    }
+
+    this.asset = (relativeUrl: Url) => assetHelper(relativeUrl, cache.config);
+    this.currentUrl = urlHelper(`${page.id}.html`, this.language.url, cache.config);
+    this.currentDefaultLanguageUrl = createAbsoluteUrl(`${page.id}.html`, cache.config);
+    this.url = (relativeUrl: Url, languageName = language.name) => urlHelper(
+      relativeUrl, languageName, cache.config);
+    this.languageUrl = (languageName: string) => urlHelper(`${page.id}.html`, languageName, cache.config);
     this.isActive = (relativeUrl: Url) =>
-      this.currentDefaultLanguageUrl === createAbsoluteUrl(relativeUrl, props.config);
+      this.currentDefaultLanguageUrl === createAbsoluteUrl(relativeUrl, cache.config);
     this.i18n = (message: PropertyPath, otherwise = ''): string => this.language.translate(message, otherwise);
-    this.link = (url: Url, content?: string, className?: string, activeClassName?: string, attrs?: Attrs, language = props.language.name): string => {
-      return HtmlFactory.createLink(this.url(url, language), content, className, activeClassName, this.isActive(url), attrs, language);
+    this.link = (
+      url: Url, content?: string, className?: string, activeClassName?: string, attrs?: Attrs, languageName = language.name): string => {
+      return HtmlFactory.createLink(this.url(url, languageName), content, className, activeClassName, this.isActive(url), attrs,
+        languageName);
     };
     this.languageLink = (language: string, text?: string, className?: string, activeClass?: string, attributes?: Attrs): string => {
-      return this.link(createAbsoluteUrl(`${this.id}.html`, props.config), text, className, activeClass, attributes, language);
+      return this.link(createAbsoluteUrl(`${page.id}.html`, cache.config), text, className, activeClass, attributes, language);
     }
 
   }
@@ -76,8 +75,8 @@ export class Page {
   id: string;
   content: string;
 
-  constructor(file: string) {
-    this.id = removeExtension(file);
-    this.content = readFileSync(file, 'utf-8');
+  constructor(public file: string) {
+    this.id = removeExtension(this.file);
+    this.content = readFileSync(this.file, 'utf-8');
   }
 }
--- lib/tasks/build.ts
+++ lib/tasks/build.ts
@@ -1,8 +1,12 @@
 import { Environment } from '../interfaces/environment';
 import { Config } from '../interfaces/config';
-import { copySync, emptyDirSync, pathExistsSync } from 'fs-extra';
+import { copySync, emptyDirSync, outputFileSync, pathExistsSync } from 'fs-extra';
 import { createBuildCache } from './cache';
 import * as path from 'path';
+import { Language } from '../entities/language';
+import { Page, PageData } from '../entities/page';
+import { BuildCache } from '../cache';
+import * as pug from 'pug';
 
 export async function build(config: Config, environment: Environment): Promise<void> {
   const cache = createBuildCache(config, environment);
@@ -14,4 +18,19 @@ export async function build(config: Config, environment: Environment): Promise<v
   if (pathExistsSync(path.join(config.src.folder, config.assets.folder))) {
     copySync(path.join(config.src.folder, config.assets.folder), config.dist.folder);
   }
+
+  buildPages(cache.pages, cache.languages, cache);
+}
+
+export function buildPages(pages: Page[], languages: Language[], cache: BuildCache) {
+  pages.forEach(page => languages.forEach(language => buildPage(page, language, cache)))
+}
+
+export function buildPage(page: Page, language: Language, cache: BuildCache) {
+  const config = cache.config;
+  const data = new PageData(page, language, cache);
+  const code = pug.render(page.content, {...data, filename: page.file});
+  const file = path.relative(path.join(config.src.folder, config.pages.folder), page.id);
+
+  outputFileSync(path.join(config.dist.folder, language.url, `${file}.html`), code);
 }
